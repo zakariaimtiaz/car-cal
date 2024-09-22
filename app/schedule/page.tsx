525a310015d6216d4abd3eb26667fcb837e4a496
Zakaria Imtiaz <imtiaz71985@gmail.com>
2024-09-22 23:57:45 +0600

    update

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { daysOfWeek, getMonthDays } from "./helpers";
 import CalNavBar from "./components/CalNavBar";
 
